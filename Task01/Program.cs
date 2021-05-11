d0df35e0a8e04566fa98ef801e822c5352c352d6
veledara <mvchesnovskiy@edu.hse.ru>
2021-05-11 19:21:14 +0300

    Task01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -2,20 +2,17 @@
 
 /*
 Источник: https://metanit.com/
-
 Как известно, неотъемлемыми компонентами бутерброда являются хлеб и масло.
 Допустим, у нас есть классы Bread, Butter, Sandwich.
 Добавьте в один из классов оператор сложения, чтобы при объединении хлеба (Bread) и масла (Butter)
 получался бутерброд (Sandwich), и, тем самым, компилировался и выполнялся без ошибок код в методе Main.
 Обработайте ситуации, когда вес отрицательный (в этом случае должен быть выброшен ArgumentException).
-
 Тестирование приложения выполняется путем запуска разных наборов тестов, например,
 на вход может поступить строка (веса компонентов бутерброда, разделенные через пробел):
 10 10
 Программа должна вывести на экран:
 20
 Никаких дополнительных символов выводиться не должно.
-
 Код метода Main можно подвергнуть изменениям, но вывод меняться не должен.
 */
 
@@ -24,6 +21,14 @@ namespace Task01
     class Bread
     {
         public int Weight { get; set; }
+        public static Sandwich operator +(Bread bread, Butter butter)
+        {
+            if (bread.Weight + butter.Weight < 0)
+            {
+                throw new ArgumentException();
+            }
+            return new Sandwich { Weight = bread.Weight + butter.Weight };
+        }
     }
     class Butter
     {
@@ -32,6 +37,7 @@ namespace Task01
     class Sandwich
     {
         public int Weight { get; set; }
+
     }
 
     class MainClass
@@ -44,12 +50,13 @@ namespace Task01
                 Bread bread = new Bread { Weight = int.Parse(strs[0]) };
                 Butter butter = new Butter { Weight = int.Parse(strs[1]) };
                 Sandwich sandwich = bread + butter;
+                Console.WriteLine(sandwich.Weight);
             }
             catch (ArgumentException)
             {
                 Console.WriteLine("error");
             }
-            Console.WriteLine(sandwich.Weight);
+
         }
     }
 }
